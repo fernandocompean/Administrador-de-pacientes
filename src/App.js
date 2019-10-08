142ef1dea0847b724a68eaec5924491d23e04410
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-10-08 01:33:17 -0500

    47. Creando el Primer Componente

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './bootstrap.min.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+class App extends Component {
+  state = { 
+
+   }
+  render() {
+    return (
+        <div className="container">
+          <Header
+            titulo='Administrador Pacientes Veterinaria'
+          />
+        </div>
+    );
+  }
 }
 
 export default App;
+
+
