3bf3150578001c1ecb784e3760b7415dff6b83f6
Macbook Air de Luis Fernando <Fer@Fernandos-MacBook-Air.local>
2019-10-08 23:38:54 -0500

    58. Mostrando un mensaje condicionalmente

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -2,24 +2,30 @@ import React from 'react';
 import Cita from './Cita';
 import PropTypes from 'prop-types';
 
-const ListaCitas = ({citas, eliminarCita}) => (
-    <div className="card mt-2 py-5">
-        <div className="card-body">
-            <h2 className="card-title text-center">
-                Administra las citas aquí
+const ListaCitas = ({citas, eliminarCita}) => {
+
+    //imprimir mensaje citas o no
+    const mensaje = Object.keys(citas).length === 0 ? 'No hay citas' : 'Administra tus citas aquí';
+
+    return (
+        <div className="card mt-2 py-5">
+            <div className="card-body">
+                <h2 className="card-title text-center">
+                    {mensaje}
             </h2>
-            <div className="lista-citas">
-                {citas.map(cita => (
-                    <Cita
-                        key={cita.id}
-                        cita={cita}
-                        eliminarCita={eliminarCita}
-                    />
-                ))}
+                <div className="lista-citas">
+                    {citas.map(cita => (
+                        <Cita
+                            key={cita.id}
+                            cita={cita}
+                            eliminarCita={eliminarCita}
+                        />
+                    ))}
+                </div>
             </div>
-        </div>  
-    </div>
-)
+        </div>
+    )
+}
 ListaCitas.protoType = {
     citas: PropTypes.array.isRequired,
     eliminarCita: PropTypes.func.isRequired
